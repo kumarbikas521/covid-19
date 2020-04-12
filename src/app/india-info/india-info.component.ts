3fd2f9b0337e29df7ad129c9380d9a1360323d62
kumarbikas521 <kumarbikas521@gmail.com>
2020-04-12 22:36:18 +0530

    alignment modified

--- src/app/india-info/india-info.component.ts
+++ src/app/india-info/india-info.component.ts
@@ -44,10 +44,12 @@ export class IndiaInfoComponent implements OnInit, AfterViewInit {
   };
   // @ViewChild(MatPaginator) paginator: MatPaginator;
   indiaDateWiseData = [];
+  deathAndRecoverData = [];
   constructor(private indiaService: IndiaApiServiceService, private datepipe: DatePipe) {
     this.indiaInfoForm = new FormGroup({
     });
     this.indiaDateWiseData = [];
+    this.deathAndRecoverData = [];
     this.indiaService.getIndiaData().subscribe((data) => {
       console.log(data);
       this.totalIndiaData = data;
@@ -58,22 +60,30 @@ export class IndiaInfoComponent implements OnInit, AfterViewInit {
           date: this.datepipe.transform(new Date(ele.date + '2020'), 'yyyy-MM-dd'),
           value: parseInt(ele.dailyconfirmed, 10)
         };
+        const deathrecoverObj = {
+          date: this.datepipe.transform(new Date(ele.date + '2020'), 'yyyy-MM-dd'),
+          death: parseInt(ele.dailydeceased, 10),
+          recover: parseInt(ele.dailyrecovered, 10)
+        };
         this.indiaDateWiseData.push(dateValue);
+        this.deathAndRecoverData.push(deathrecoverObj);
+
       });
       console.log(this.indiaDateWiseData);
       this.ngAfterViewInit();
-      // this.totalIndiaData.statewise.paginator = this.paginator;
+      this.plotDeathRecoverGraph();
     });
   }
 
   ngOnInit() {
+
 
   }
   ngAfterViewInit() {
 
     // Create chart instance
     const chart = am4core.create('chartdiv', am4charts.XYChart);
-   // chart.scrollbarX = new am4core.Scrollbar();
+    // chart.scrollbarX = new am4core.Scrollbar();
     chart.data = this.indiaDateWiseData;
     // Create axes
     const categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
@@ -114,6 +124,50 @@ export class IndiaInfoComponent implements OnInit, AfterViewInit {
     });
 
     // Cursor
+    chart.cursor = new am4charts.XYCursor();
+  }
+  plotDeathRecoverGraph() {
+    const chart = am4core.create('chartdivDeath', am4charts.XYChart);
+    chart.data = this.deathAndRecoverData;
+    // Create axes
+    const categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = 'date';
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 30;
+    categoryAxis.renderer.labels.template.horizontalCenter = 'right';
+    categoryAxis.renderer.labels.template.verticalCenter = 'middle';
+    categoryAxis.renderer.labels.template.rotation = 270;
+    categoryAxis.tooltip.disabled = true;
+    categoryAxis.renderer.minHeight = 110;
+
+    // Create value axis
+    const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.baseValue = 0;
+
+    // Create series
+    const series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = 'death';
+    series.dataFields.categoryX = 'date';
+    series.strokeWidth = 2;
+    series.tensionX = 0.77;
+    series.stroke  = am4core.color('#FF0000');
+    // bullet is added because we add tooltip to a bullet for it to change color
+    const bullet = series.bullets.push(new am4charts.Bullet());
+    bullet.tooltipText = '{valueY}';
+
+    const series1 = chart.series.push(new am4charts.LineSeries());
+    series1.dataFields.valueY = 'recover';
+    series1.dataFields.categoryX = 'date';
+    series1.strokeWidth = 2;
+    series1.tensionX = 0.77;
+    series1.stroke  = am4core.color('#00FF00');
+    // bullet is added because we add tooltip to a bullet for it to change color
+    const bullet1 = series1.bullets.push(new am4charts.Bullet());
+    bullet1.tooltipText = '{valueY}';
+
+
+
+
     chart.cursor = new am4charts.XYCursor();
   }
 }
