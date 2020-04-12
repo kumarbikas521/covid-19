a364c91d07eff37076df9f8ce30025c31d5d130a
kumarbikas521 <kumarbikas521@gmail.com>
2020-04-12 20:27:56 +0530

    alignment modified

--- src/app/services/india-api-service.service.ts
+++ src/app/services/india-api-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as data from '../../assets/URL.json';
-import { from } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
